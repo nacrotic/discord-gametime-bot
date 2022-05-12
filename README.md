0d8b8730962efd7f095a447c072dad53c1c7e97e
leo <leo.perrin@gmail.com>
2022-05-12 22:41:39 +0200

    Commit Initial

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # discord-gametime-bot
 Bot discord pour mon association de jeux de société
+
+Objectifs :
+1. [ ] Message d'accueil personnalisé pour les nouveaux arrivants
+2. [ ] Plannification des rencontre de l'association avec des reactions par defaut
+3. [ ] Ajout d'une thématique aux sessions
+4. [ ] Statistique de participation directement sur le message
---
+++ main.py
@@ -0,0 +1,35 @@
+from discord import Intents
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path="config")
+
+
+class MyBot(commands.Bot):
+    def __init__(self):
+        default_intents = Intents.default()
+        default_intents.members = True
+        super().__init__(command_prefix="!", intents=default_intents)
+
+    async def on_ready(self):
+        print("on_ready bot")
+
+    async def on_message(self, message):
+        print("on_message " + message.content)
+
+    async def on_member_join(self, member):
+        print("on_member_join " + member.display_name)
+        general_channel = bot.get_channel(int(os.getenv("channel")))
+        await general_channel.send(f"Bienvenue sur le serveur {member.display_name} !")
+
+    @commands.Bot.command(name="test")
+    async def test(self, ctx, number: int):
+        print("on_message " + str(number))
+        messages = await ctx.channel.history(limit=number + 1).flatten()
+        for each_message in messages:
+            await each_message.delete()
+
+
+bot = MyBot()
+bot.run(os.getenv("token"))
