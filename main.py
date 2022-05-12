ea54d2dccf16431cdcc8d7d0c56bfac105feec22
leo <leo.perrin@gmail.com>
2022-05-12 22:49:47 +0200

    Commit Initial

--- main.py
+++ main.py
@@ -4,32 +4,39 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv(dotenv_path="config")
+default_intents = Intents.default()
+default_intents.members = True
+bot = commands.Bot(command_prefix="!", intents=default_intents)
 
 
-class Bot(commands.Bot):
-    def __init__(self):
-        default_intents = Intents.default()
-        default_intents.members = True
-        super().__init__(command_prefix="!", intents=default_intents)
-
-    async def on_ready(self):
-        print("on_ready bot")
-
-    async def on_message(self, message):
-        print("on_message " + message.content)
-
-    async def on_member_join(self, member):
-        print("on_member_join " + member.display_name)
-        general_channel = bot.get_channel(int(os.getenv("channel")))
-        await general_channel.send(f"Bienvenue sur le serveur {member.display_name} !")
-
-    @commands.Bot.command(name="test")
-    async def test(self, ctx, number: int):
-        print("on_message " + str(number))
-        messages = await ctx.channel.history(limit=number + 1).flatten()
-        for each_message in messages:
-            await each_message.delete()
+def connect():
+    bot.run(os.getenv("token"))
 
 
-bot = Bot()
-bot.run(os.getenv("token"))
+@bot.event
+async def on_ready():
+    print("on_ready bot")
+
+
+@bot.command(name="test")
+async def test(ctx, number: int):
+    print("on_message " + str(number))
+    messages = await ctx.channel.history(limit=number + 1).flatten()
+    for each_message in messages:
+        await each_message.delete()
+
+
+@bot.event
+async def on_message(message):
+    print("on_message " + message.content)
+
+
+@bot.event
+async def on_member_join(member):
+    print("on_member_join " + member.display_name)
+    general_channel = bot.get_channel(os.getenv("channel"))
+    await general_channel.send(f"Bienvenue sur le serveur {member.display_name} !")
+
+
+
+connect()
